552c6ae24bcb52353530c7c264a8b4651cd6aa23
Arsentyeva N.V <marina.vyy@mail.ru>
2022-11-01 19:04:07 +0900

    попытка поменять кодировку

--- ClassSquareFig/SquareFig.cpp
+++ ClassSquareFig/SquareFig.cpp
@@ -1,10 +1,10 @@
 // @author: Natalya Arsentyeva
 #include "SquareFig.h"
 #include "math.h"
-#define _USE_MATH_DEFINES // âêë. ìàòåìàòè÷åñêèå êîíñòàíòû
+#define _USE_MATH_DEFINES // Вкл. мат. константы
 
 
-/// Êîíñòðóêòîð ïî óìîë÷àíèþ
+/// Конструктор по умолчанию
 SquareFig::SquareFig() {
 	ax = 0;
 	ay = 0;
@@ -22,18 +22,18 @@ SquareFig::SquareFig(float x1, float y1, float x2, float y2, float x3, float y3,
 }
 
 
-/// Çàäàòü êîîðäèíàòû
+/// Задание сторон
 void SquareFig::set_coordinates(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4) {
 	float s1, s2, s3, s4, d1, d2;
-	// Ñòîðîíû
+	// Стороны
 	s1 = pow((x2 - x1), 2) + pow((y2 - y1), 2);
 	s2 = pow((x3 - x2), 2) + pow((y3 - y2), 2);
 	s3 = pow((x4 - x3), 2) + pow((y4 - y3), 2);
 	s4 = pow((x1 - x4), 2) + pow((y1 - y4), 2);
-	// Äèàãîíàëè
+	// Диагонали
 	d1 = pow((x3 - x1), 2) + pow((y3 - y1), 2);
 	d2 = pow((x4 - x2), 2) + pow((y4 - y2), 2);
-	// Ñðàâíåíèå äëèí ñòîðîí è äèàãîíàëåé
+	// Проверка на квадрат
 	if ((fabs(s1 - s2) > FLT_EPSILON) || (fabs(s1 - s3) > FLT_EPSILON) || (fabs(s1 - s4) > FLT_EPSILON) || (fabs(d1 - d2) > FLT_EPSILON)) throw std::invalid_argument("Error: sides are set incorrectly, square does not exist");
 	ax = x1;
 	ay = y1;
@@ -45,22 +45,22 @@ void SquareFig::set_coordinates(float x1, float y1, float x2, float y2, float x3
 	dy = y4;
 }
 
-/// Íàõîæäåíèå äëèíû îäíîé ñòîðîíû
+/// Нахождение стороны
 float SquareFig::side_lengths() const {
 	return sqrt((pow((bx - ax), 2)) + pow((by - ay), 2));
 }
 
-/// Íàõîæäåíèå ïåðèìåòðà
+/// Нахождение периметра
 float SquareFig::perimeter() const {
 	return sqrt((pow((bx - ax), 2)) + pow((by - ay), 2)) * 4;
 }
 
-/// Íàõîæäåíèå ïëîùàäè
+/// Нахождение площади
 float SquareFig::area() const {
 	return pow(sqrt((pow((bx - ax), 2)) + pow((by - ay), 2)), 2);
 }
 
-/// Ïåðåâîä â ñòðîêó
+/// Преобразование в строку
 std::string SquareFig::to_string() const {
 	return "lengths side = " + std::to_string(side_lengths()) + " cm; P = " + std::to_string(perimeter()) + " cm; S = " + std::to_string(area()) + " cm2";
 }
--- SquareFig-test/test.cpp
+++ SquareFig-test/test.cpp
@@ -6,12 +6,10 @@
 TEST(TestClassSquareFig, TestSideLen) {
 	SquareFig f1(2, 2, 2, 6, -2, 6, -2, 2);
 	EXPECT_EQ(f1.side_lengths(), 4);
-	SquareFig f3(5, 3, 2.5, 3, 2.5, 0.5, 5, 0.5);
-	EXPECT_EQ(f3.side_lengths(), 2.5);
-	//SquareFig f2(0, 3, 3, 0, 0, -3, -3, 0);
-	//EXPECT_EQ(f2.side_lengths(), fabs(f2.side_lengths()-4.24264) < FLT_EPSILON);
-	//SquareFig f4(1, 3, 2.5, 3, 2.5, 0.5, 5, 0.5);
-	//EXPECT_EQ(f4.side_lengths(), invalid_argument("Error: sides are set incorrectly, square does not exist"));
+	SquareFig f2(5, 3, 2.5, 3, 2.5, 0.5, 5, 0.5);
+	EXPECT_EQ(f2.side_lengths(), 2.5);
+	//SquareFig f3(1, 3, 2.5, 3, 2.5, 0.5, 5, 0.5);
+	//EXPECT_EQ(f3.side_lengths(), invalid_argument("Error: sides are set incorrectly, square does not exist"));
 
 	// todo
 }
